1d454094b02d808a96702e4b683ab3453f276ae5
Oasis14 <hkyryan14@gmail.com>
2019-06-26 22:13:53 -0500

    adding basic template for html. Starting to learn bootstrap for the css to help ease the web development

--- webserver/pages/views.py
+++ webserver/pages/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
-from django.http import HttpResponse
-
-def homePageView(request):
-    return HttpResponse("Hello, World!")
+class HomePageView(TemplateView):
+    template_name ='home.html'
